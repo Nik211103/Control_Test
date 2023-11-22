b3bd71cf0d24f146f40d2c3964d59948f31c955b
Nikita <mitichkin0303@mail.ru>
2023-11-22 23:55:45 +0200

    Придумал использовать лямбда-функцию(предтестовая версия)

--- Control_Test.py
+++ Control_Test.py
@@ -2,12 +2,17 @@
 # длина которых меньше, либо равна 3 символам. 
 # Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 # При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+# def filter_strings(strings):
+#     new_array = []
+#     for string in strings:
+#         if len(string) <= 3:
+#             new_array.append(string)
+#     return new_array
+
 def filter_strings(strings):
-    new_array = []
-    for string in strings:
-        if len(string) <= 3:
-            new_array.append(string)
-    return new_array
+    return list(filter(lambda s: len(s) <= 3, strings))
 
 input_array = input("Введите элементы массива через пробел: ").split()
 result_array = filter_strings(input_array)
