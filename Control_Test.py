202cdfc068ab44e85310acfd19b40c57dc28524f
Nikita <mitichkin0303@mail.ru>
2023-11-22 23:53:44 +0200

    Решил попробовать через цикл for, но думаю что можно придумать что-то получше

--- Control_Test.py
+++ Control_Test.py
@@ -2,8 +2,12 @@
 # длина которых меньше, либо равна 3 символам. 
 # Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 # При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
-def filter_strings(string):
-    pass
+def filter_strings(strings):
+    new_array = []
+    for string in strings:
+        if len(string) <= 3:
+            new_array.append(string)
+    return new_array
 
 input_array = input("Введите элементы массива через пробел: ").split()
 result_array = filter_strings(input_array)
